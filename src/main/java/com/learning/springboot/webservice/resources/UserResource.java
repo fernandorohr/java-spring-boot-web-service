37931debeae1f4601c22af8800c18b66f0a20556
fernandorohr <fernando.felipe.flesch@gmail.com>
2020-11-19 11:49:04 -0300

    user entity/resource implemented

---
+++ src/main/java/com/learning/springboot/webservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.learning.springboot.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learning.springboot.webservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "999999999", "1234567890");
+		return ResponseEntity.ok().body(user);
+	}
+}
